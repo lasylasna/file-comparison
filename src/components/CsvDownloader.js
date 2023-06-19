7d4a7566d19981d52af8b5cc7714802cf1567f56
lasylasna <lasylasna@gmail.com>
2023-06-20 10:49:07 +1200

    first commit

---
+++ src/components/CsvDownloader.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class CsvDownloader extends React.Component {
+  downloadCsv = () => {
+    const { dataArray, fileName } = this.props;
+
+    const csvData = dataArray.map(obj => Object.values(obj).join(',')).join('\n');
+    const csvBlob = new Blob([csvData], { type: 'text/csv;charset=utf-8;' });
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = URL.createObjectURL(csvBlob);
+    downloadLink.download = fileName;
+    downloadLink.click();
+  };
+
+  downloadText=()=>{
+    const { dataArray, fileName } = this.props;
+    const text =dataArray.map(obj => Object.values(obj).join('  ')).join('\n');
+    const blob = new Blob([text], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'Data.txt';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  }
+
+  render() {
+    return (
+      <button  className="button-71" role="button"  onClick={this.downloadText}>
+        Click to Download
+      </button>
+    );
+  }
+}
+
+export default CsvDownloader;
---
+++ src/components/Fileconvertor.js
@@ -0,0 +1,195 @@
+import React, { useEffect, useRef, useState } from "react"; 
+import CsvDownloader from "./CsvDownloader";
+import csvtojson from "csvtojson";
+import data from "./data.json"; 
+import upload from "./images/upload.png"; 
+import "./Header.css"; 
+
+const Fileconvertor = () => {
+  const [jsonData, setJsonData] = useState(null);
+  const [csvJsonData, setCsvJsonData] = useState(data);
+  const [dataArray, setDataArray] = useState([]);
+  const [fileName, setFileName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [conversionArray, setConversionArray] = useState([]);
+  const [loadingMessage, setLoadingMessage] = useState(`Please click the "Click to add SortInfo" and wait untill it change to "Download"...`);
+  const [loadingMessage2, setLoadingMessage2] = useState(` Don't close the window, it may takes few minutes depending upon the speed of your internet conncetion`)
+  const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    if (conversionArray.length > 0) {
+      setLoading(true);
+    }
+  }, [conversionArray.length]);
+
+  useEffect(() => {
+    if (dataArray.length > 0) {
+      setLoading(false);
+    }
+  }, [dataArray.length]);
+
+  const handleFileUpload = (event) => {
+    setDataArray([]);
+    const file = event.target.files[0];
+    setFileName(file.name);
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const text = e.target.result;
+      const jsonData = convertToJSON(text);
+      setJsonData(jsonData);
+    };
+    reader.readAsText(file);
+  };
+
+  const handleCsvFileUpload = (event) => {
+    const file = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = async (e, index) => {
+      const text = e.target.result;
+      const tempJsonData = await csvtojson().fromString(text);
+      setCsvJsonData(tempJsonData);
+    };
+
+    reader.readAsText(file);
+  };
+
+  const convertToJSON = (text) => {
+    const lines = text.replace(/\r/g, "").split("\n");
+    const keys = lines[0].split("\t");
+    const data = [];
+
+    for (let i = 1; i < lines.length; i++) {
+      const values = lines[i].split("\t");
+      const obj = {};
+
+      for (let j = 0; j < keys.length; j++) {
+        obj[keys[j]] = values[j];
+      }
+
+      data.push(obj);
+    }
+
+    return data;
+  };
+
+  const handleClick = async() => { 
+      await filterIfSubset(csvJsonData, jsonData);
+     // updateState(); 
+  };
+ 
+  
+   const  filterIfSubset=async(csv, textData)=>{
+    let tempArray = [
+      {
+        Address1Postal: "Address1Postal",
+        Address2Postal: "Address2Postal",
+        Address3Postal: "Address3Postal",
+        Address4Postal: "Address4Postal",
+        Address5Postal: "Address5Postal",
+        PostCode: "PostCode",
+        SortInfo: "SortInfo",
+      },
+    ];
+    let finalArray = [];
+    let temp = {};
+    console.log(csv.length, textData.length);
+    //let tempPercentage;
+     //const subset = data.slice(0, 10);
+    textData.map((text, i) => {
+      temp = {
+        Address1Postal: text.Address1Postal,
+        Address2Postal: text.Address2Postal,
+        Address3Postal: text.Address3Postal,
+        Address4Postal: text.Address4Postal,
+        Address5Postal: text.Address5Postal,
+        PostCode: text.PostCode,
+        SortInfo: text.PostCode,
+      };
+      tempArray[i + 1] = temp;
+      //conversionArray.push(tempArray)
+      //updateArrayLength(tempArray);
+      for (let j = 0; j < csv.length; j++) {
+        if (
+          (
+            csv[j].HOUSENUMBER +
+            " " +
+            csv[j].STREETNAME +
+            " " +
+            csv[j].CUSTOM1
+          ).toUpperCase() == text.Address1Postal.toUpperCase() &&
+          parseInt(csv[j].POSTCODE) == parseInt(text.PostCode)
+        ) {
+          tempArray[i + 1]["SortInfo"] = csv[j].SORTINFO;
+
+          //conversionArray.push(tempArray)
+         // updateArrayLength(tempArray);
+          break;
+        }
+      }
+    });
+
+    //}
+    finalArray = tempArray;
+    setDataArray(tempArray);
+console.log(dataArray)
+    return dataArray;
+  }
+
+  return (
+    <div className="header-div"> 
+    {dataArray.length > 0 ? (
+   null
+    ): (fileName && 
+    <div className="header-loading-message">
+    <p>{loadingMessage} </p>
+    <p>{loadingMessage2} </p>
+    </div>)
+    }
+ 
+   
+      <div className="file-uploder">
+        <div>
+
+            <div>
+              <div className="image-upload">
+            
+                <label htmlFor="file-input">
+                  <img src={upload} id="upload-img" />
+                </label>
+
+                <input
+                  type="file"
+                  accept=".txt"
+                  onChange={handleFileUpload}
+                  id="file-input"
+                  name="file-input"
+                />
+                <label htmlFor="file-input">{fileName}</label>
+              </div>
+              {dataArray.length > 0 ? (
+                <div>
+                  <CsvDownloader dataArray={dataArray} fileName="data.csv" />
+                </div>
+              ) : (
+                <div>
+                  {fileName && (
+                    <button
+                      className="button-71"
+                      role="button"
+                      onClick={handleClick}
+                    >
+                      Click to add SortInfo
+                    </button>
+                  )}
+                </div>
+              )}
+            </div>
+          
+        </div>{" "}
+      </div>
+    </div>
+  );
+};
+
+export default Fileconvertor;
